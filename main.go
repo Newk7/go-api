a9fbf5f478830948c31f058b6237e82d4e6cf1e9
Kevin Newcomb <kevin@sirrus7.com>
2023-03-20 16:16:56 -0400

    reinit

--- main.go
+++ main.go
@@ -47,6 +47,7 @@ func main() {
 func jsonContentTypeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
 }
